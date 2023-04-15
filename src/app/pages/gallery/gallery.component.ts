f65cab6a976d7b8ae7009e6c1460e9d2f3ed65a0
Csapó Krisztina <h982321@stud.u-szeged.hu>
2023-04-15 16:01:36 +0200

    gg

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { GalleryObject } from '../../shared/constants/constants';
+import { GalleryObject } from '../../shared/constants/constants'; 
 
 @Component({
   selector: 'app-gallery',
